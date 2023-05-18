eea0dd81a7cdbf18a0dfa62d0b6e7bf6e8e212ba
marianalua <yuzukisilva3@gmail.com>
2023-05-18 11:04:49 -0300

    Entendi a flavia não

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         // Aula introdutória de Orientação á Objetos
+        // Declaração de objeto
+        Pessoa adao;
+        // Instanciação do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquel = new Pessoa();
+        // Definir formato do objeto
+        qualquel.nome = "Rita";
+        qualquel.sobrenome = "Lee";
+        qualquel.falar();
+        System.out.println(qualquel.falar("oi"));
 
     }
 }
